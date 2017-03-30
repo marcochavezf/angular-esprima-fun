c1331a42e5ae18d8137a854c9fe791bdd4a569dc
Marco Chávez <marcochavezf@hotmail.com>
2017-03-30 09:38:20 -0600

    browserify lib and create a dist bundle
    add a function to create semantics from a given array of source code

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,28 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+
+gulp.task('dist', function () {
+  // set up the browserify instance on a task basis
+
+  return browserify({ entries: './lib/index.js',  debug: true, standalone: 'angular-esprima-fun'})
+    .transform('babelify', { presets: ['es2015'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    // Add transformation tasks to the pipeline here.
+      //.pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});
 
 gulp.task('static', function () {
   return gulp.src('**/*.js')
@@ -45,8 +67,8 @@ gulp.task('test', ['pre-test'], function (cb) {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
+  gulp.watch(['lib/**/*.js', 'test/**'], ['test', 'dist']);
 });
 
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+gulp.task('default', ['static', 'test', 'dist']);
