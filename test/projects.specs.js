69c33cb12d04cd082c48e14d68c1253985a688dc
Marco Chávez <marcochavezf@hotmail.com>
2017-04-19 10:25:41 -0500

    parse files generated from typescript files

--- test/projects.specs.js
+++ test/projects.specs.js
@@ -9,13 +9,38 @@ var helperTest = require('./helpers');
 var enableVerbose = false;
 
 describe('Projects', function(){
-  it('should parse a project containing all kind of angular components', function (done) {
+  it('should parse a project containing all kind of angular components (Project A)', function (done) {
     this.timeout(60000);
 
     var projectContentA = require('./file.examples/testC/project-a-example.json');
     angularEsprimaFun.createSemanticsFromSrc({ pathAndSrcFiles: projectContentA.srcContent }, (projectSemantics)=>{
       projectContentA.srcContent.length.should.equal(projectSemantics.filesParsed.length);
       //TODO: add more cases to test
+      done();
+    }, enableVerbose);
+  });
+
+  it('should parse a project containing all kind of angular components (Project B)', function (done) {
+    this.timeout(60000);
+
+    var projectContentA = require('./file.examples/testC/project-b-example.json');
+    angularEsprimaFun.createSemanticsFromSrc({ pathAndSrcFiles: projectContentA.srcContent }, (projectSemantics)=>{
+      projectContentA.srcContent.length.should.equal(projectSemantics.filesParsed.length);
+      //TODO: add more cases to test
+      done();
+    }, enableVerbose);
+  });
+
+  it('should parse directives from file.examples/testD/example5.js (local files)', function (done) {
+    if (enableVerbose) {
+      this.timeout(5000);
+    }
+
+    var dirTest = 'test/file.examples/testD/example5.js';
+    angularEsprimaFun.createProjectSemantics(dirTest, (projectSemantics)=>{
+      projectSemantics.controllersSemantics.should.have.length(2);
+      projectSemantics.directivesSemantics.should.have.length(5);
+      projectSemantics.servicesSemantics.should.have.length(3);
       done();
     }, enableVerbose);
   });
