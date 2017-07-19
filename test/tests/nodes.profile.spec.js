5a9b8358b23bbcd38564378ef6bab72ea833c4e3
Marco Chávez <marcochavezf@hotmail.com>
2017-07-19 12:54:36 -0500

    add unit test for cpuprofile filtering

--- test/tests/nodes.profile.spec.js
+++ test/tests/nodes.profile.spec.js
@@ -1,12 +1,24 @@
 /**
  * Created by marcochavezf on 5/4/17.
  */
+var fs = require('fs');
 var chai = require('chai');
 var should = chai.should();
 var angularEsprimaFun = require('../../lib');
 
 describe('Test JS Profile', function () {
-  it('testing with profile file', function(done){
+  it('testing with a manually generated profile file', function(done){
+    var cpuProfilePath = 'test/file.examples/testE/profileA.json';
+    var pathToFilter = 'good';
+    angularEsprimaFun.getProjectNodesFromProfile({ cpuProfilePath, pathToFilter }, function(projectNodes, error){
+      var expectedOutPutText = fs.readFileSync('test/file.examples/testE/profileB.json', 'utf8');
+      var expectedOutPut = JSON.parse(expectedOutPutText);
+      projectNodes.should.to.deep.equal(expectedOutPut.nodes);
+      done(error);
+    })
+  });
+
+  it.skip('testing with a real profile file', function(done){
     this.timeout(100000);
     var cpuProfilePath = 'test/prototype/CPU-20161215T223525.cpuprofile';
     var pathToFilter = 'http://dev.primotus.com:8080/app';
