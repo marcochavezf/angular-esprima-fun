bb1805e40c86e2501591143ed1546250dd2d8e5c
Marco Chávez <marcochavezf@hotmail.com>
2017-10-16 18:04:25 -0500

    add unit tests for components

--- test/file.examples/testF/example2.js
+++ test/file.examples/testF/example2.js
@@ -5,5 +5,13 @@ angular.module('heroApp').component('heroDetail', {
 	templateUrl: 'heroDetail.html',
 	bindings: {
 		hero: '='
+	},
+	controller: function($scope) {
+		$scope.abc = 'abc';
+		$scope.bcd = function(){};
+		$scope.def = def;
+
+		function def(){
+		}
 	}
 });
--- test/tests/components.spec.js
+++ test/tests/components.spec.js
@@ -11,19 +11,35 @@ var enableVerbose = false;
 
 describe('Components', function () {
 
-	it.only('should parse component from file.examples/testF (local files)', function (done) {
-		if (enableVerbose) {
-			this.timeout(5000);
-		}
+	it('should parse component from file.examples/testF (local files)', function (done) {
+		this.timeout(5000);
 
 		var dirTest = 'test/file.examples/testF';
 		angularEsprimaFun.createProjectSemantics(dirTest, (projectSemantics)=> {
 			var componentsSemantics = projectSemantics.componentsSemantics;
-			var directive = directivesSemantics[0];
-			directive.name.should.equal('dateWidget');
-			directive.returnStatement.argument.properties.should.have.length(5);
+
+			var componentA = componentsSemantics[0];
+			componentA.name.should.equal('heroDetail');
+			componentA.object.properties.should.have.length(3);
+			componentA.controller.scopeProperties.should.have.length(1);
+			componentA.controller.scopeFunctions.should.have.length(2);
+
+			var componentB = componentsSemantics[1];
+			componentB.name.should.equal('counter');
+			componentB.object.properties.should.have.length(2);
+			componentB.controller.thisFunctions.should.have.length(2);
+
+			var componentC = componentsSemantics[2];
+			componentC.name.should.equal('groupComponent');
+			componentC.object.properties.should.have.length(4);
+			componentC.controller.thisProperties.should.have.length(1);
+
+			var componentD = componentsSemantics[3];
+			componentD.name.should.equal('groupComponent2');
+			componentD.object.properties.should.have.length(4);
+			componentD.controller.thisProperties.should.have.length(1);
+
 			done();
 		}, enableVerbose);
 	});
-
 });
